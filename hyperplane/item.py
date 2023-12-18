97921b696f71979fc6b67f11cb811fbbdb966238
kramo <93832451+kra-mo@users.noreply.github.com>
2023-12-18 12:58:59 +0100

    Remove extra whitespace
    Editing on mobile is hard :p

--- hyperplane/item.py
+++ hyperplane/item.py
@@ -127,7 +127,7 @@ class HypItem(Adw.Bin):
             self.thumbnail.add_css_class(self.color + "-background")
             self.extension_label.add_css_class(self.color + "-extension")
             return
-    
+
         self.thumbnail.add_css_class("gray-background")
         self.extension_label.add_css_class(self.color + "-extension-thumb")
 
